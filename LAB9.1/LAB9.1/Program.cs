2beac0a17b89805df60c51549fb5bff8a8f8403d
Vitos <rustamminis@gmail.com>
2025-12-24 20:24:11 +0200

    Add files via upload

---
+++ LAB9.1/LAB9.1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите координату x: ");
+double x = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y: ");
+double y = double.Parse(Console.ReadLine()!);
+Perimetr perimetr = new Perimetr(x,y);
+perimetr.Print();
+class Perimetr
+{
+    private double X;
+    private double Y;
+    public Perimetr(double X, double Y)
+    {
+        this.X = X;
+        this.Y = Y;
+    }
+    public double getX() { return X; }
+    public double getY() { return Y; }
+    public double getP()
+    {
+        return 2*X + 2*Y;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Периметр прямоугольника: {getP()}");
+    } 
+}
